34f2e0042a7cd476257ec5f10ff6dd890bf32366
webiovg <kevin.kowalsky1@gmail.com>
2024-01-04 11:23:49 +0100

    Identity class

---
+++ src/main/java/tp2/Identity.java
@@ -0,0 +1,31 @@
+package tp2;
+
+public class Identity {
+    private String firstName;
+    private String lastName;
+    private Address address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Address getAddress() {
+        return adress;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
